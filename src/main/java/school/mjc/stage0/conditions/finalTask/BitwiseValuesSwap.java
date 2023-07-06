728121d2d3a47efe2600152d1e581fcd27050145
NeverRIEght <lfkomarovme@gmail.com>
2023-07-06 13:57:48 +0300

    Fix Bitwise values swap Problem solved: typo in main formula

--- src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
@@ -4,7 +4,7 @@ public class BitwiseValuesSwap {
     public void swap(int first, int second) {
         first = first ^ second;
         second = second ^ first;
-        first = first ^ first;
+        first = first ^ second;
 
         System.out.println(first);
         System.out.println(second);
